2696068acf544ff011ee7903398e8cc8ddae93d5
Jahidul <jahidul.cf@gmail.com>
2026-03-07 21:07:36 +0600

    Dashboard static design created

--- login.js
+++ login.js
@@ -12,6 +12,7 @@ loginFrom.addEventListener('submit', (e) => {
 
 const login = (username, password) => {
     if (username === 'admin' && password === 'admin123') {
+        
         window.location.href = 'dashboard.html';
     } else {
         window.location.href = 'login.html';
